927fbbf978eea0b800492a969ae3565f3380adbc
pvl <ivanov.pavel.1307@gmail.com>
2023-11-21 17:02:45 +0300

    fixed part

---
+++ src/file_readers.py
@@ -0,0 +1,23 @@
+import os
+from typing import Union
+
+import pandas as pd
+
+
+def read_csv(filename: str) -> Union[pd.DataFrame, None]:
+    """Чтение CSV файла и вывод транзакций"""
+    csv_file_path = os.path.join(os.path.dirname(__file__), '..', 'data', filename)
+
+    reader = pd.read_csv(csv_file_path, delimiter=';')
+    return reader
+
+
+def read_xlsx(filename: str) -> Union[pd.DataFrame, None]:
+    """Чтение XLSX файла и вывод транзакций"""
+    xlsx_file_path = os.path.join(os.path.dirname(__file__), '..', 'data', filename)
+
+    reader = pd.read_excel(xlsx_file_path)
+    return reader
+
+# print(read_csv('transactions.csv'))
+# print(read_xlsx('transactions_excel.xlsx'))
