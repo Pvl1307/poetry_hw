ef3b55d03e0dbf7f049eeb5ae2936ddc0892de1b
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-11-23 11:09:54 +0300

    search_transaction func

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+import re
 from typing import List, Dict, Any
 
 
@@ -24,17 +25,13 @@ def sort_dict_list_by_date(data: List[Dict[str, Any]], reverse: bool = False) ->
     return data
 
 
-data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
+def search_transaction(transactions: list[dict[Any, Any]], search: str) -> list[dict[Any, Any]]:
+    """Поиск операций по определенной строке и вывод этих операций"""
+    result = []
+    pattern = re.compile(search, re.IGNORECASE)
 
-state = 'CANCELED'
+    for transaction in transactions:
+        if re.search(pattern, transaction.get('description')):
+            result.append(transaction)
 
-# Вывод операций с определенным статусом
-print(get_state_of_operation(data, state))
-
-# Сортировка по дате в убывающем порядке
-print(sort_dict_list_by_date(data, reverse=True))
+    return result
